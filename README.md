e5f55f852cdf23e15ca3b79c920dcf02379944fa
MuhammadAhhadQumar <ahhadqamar1@gmail.com>
2026-08-20 19:42:44 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# StudySight
+# StudySight
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from extensions import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=false)
+    role = db.Column(db.String(20), nullable=False, default="user")
+
+
+class Students(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    roll_number = db.Column(db.String(20), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    class_name = db.Column(db.String(50), nullable=False)
