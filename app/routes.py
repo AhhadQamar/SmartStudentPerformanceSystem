d980f24b52c8edeb1d13c7cf67c007a5d2581c60
AhhadQamar <ahadqam1@gmail.com>
2026-08-22 21:47:28 +0500

    implement routing functionalites to different dashboards according to the user role

--- app/routes.py
+++ app/routes.py
@@ -9,33 +9,41 @@ from .models import Students, User
 main = Blueprint("main", __name__)
 
 
-@main.route("/")
-@main.route("/home")
-def index():
-    return render_template("index.html")
-
 # function to check is the user is logged in or not. If not logged in, redirect to login page. If logged in, allow access to the view function.
 def login_required(view):
     @wraps(view)
-    def wrapped_view(**args,*kwargs):
+    def wrapped_view(*args, **kwargs):
         if "user_id" not in session:
-            return redirect(url_for('main.login'))
-        return view(**args, **kwargs)
+            return redirect(url_for("main.login"))
+        return view(*args, **kwargs)
+
     return wrapped_view
+
 
 # fucntion to check if the user has the required role to access the view . If not, redirect to the index page. If yes, allow access to the the view
 def role_required(*allowed_roles):
     def decorator(view):
         @wraps(view)
-        def wrapped_view(*args,**kwargs):
-            if "role" not in session or session['role'] not in allowed_roles:
-                flash('You do not have permission to access this page.', 'danger')
-                return redirect(url_for('main.index'))
-            return view(*args,**kwargs)
-        return wrapped_view
-    return decorator    
+        def wrapped_view(*args, **kwargs):
+            if "role" not in session or session["role"] not in allowed_roles:
+                flash("You do not have permission to access this page.", "danger")
+                return redirect(url_for("main.index"))
+            return view(*args, **kwargs)
 
-# setting up main route and making sure user is logged in and is saved in session and has the correct role
+        return wrapped_view
+
+    return decorator
+
+
+# setting up main route
+@main.route("/")
+@main.route("/home")
+def index():
+    return render_template("index.html")
+
+
+# setting up the log in route making sure user is logged in and is saved in session and has the correct role
+
 
 @main.route("/login", methods=["GET", "POST"])
 def login():
@@ -48,14 +56,27 @@ def login():
             session["user_id"] = user.id
             session["username"] = user.username
             session["role"] = user.role
-            return redirect(url_for("main.index"))
+            return redirect(url_for("main.dashboard"))
 
         flash("Invalid username or password", "danger")
 
     return render_template("login.html")
+
 
 # route for the loggin out of the app. This will clear the session and redirect to the login page
 @main.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("main.login"))
+
+
+@main.route("/dashboard")
+@login_required
+def dashboard():
+    role = session.get("role")
+    if role == "Administrator":
+        return render_template("admin_dashboard.html")
+    if role == "teacher":
+        return render_template("teacher_dashboard.html")
+    flash("You do not have permission to access this page.", "danger")
+    return redirect(url_for("main.index"))
